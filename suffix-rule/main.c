e225dd05c8d6faa46ed4a1a0a33d7d6427019969
whj2819 <whj2819@gmail.com>
2014-03-28 14:34:53 +0800

    nouse

--- suffix-rule/main.c
+++ suffix-rule/main.c
@@ -1,50 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void f1()
+
+int main(int argc, const char *argv[])
 {
-    int i;
-    int *p;
-    for (i = 0; i < 100; i++) {
-        p = malloc(sizeof(int));
-        *p = 10;
-        free(p);
-    }
-}
-
-void f2()
-{
-    int i;
-    int *p;
-    for (i = 0; i < 10; i++) {
-        p = malloc(sizeof(int));
-        *p = 10;
-        free(p);
-    }
-}
-
-void f3()
-{
-    int i;
-    int *p;
-    for (i = 0; i < 50; i++) {
-        p = malloc(sizeof(int));
-        *p = 10;
-        free(p);
-    }
-}
-
-int main()
-{
-    int i;
-
-    for (i = 0; i < 1000000; i++) {
-        f1();
-        f2();
-        f3();
-    }
-
-
+    printf("hello world \n");
 
     return 0;
 }
